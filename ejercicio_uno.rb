39c7c32e8c05be663be7289f07643e389bee2823
santi <santiagodelafuente2013@gmail.com>
2022-03-15 16:36:48 -0300

    Initial commit

---
+++ ejercicio_uno.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+def fizz_buzz(n)
+  (1..n).each do |number|
+    if (number % 3).zero? && (number % 5).zero?
+      puts 'FizzBuzz'
+    elsif (number % 3).zero?
+      puts 'Fizz'
+    elsif (number % 5).zero?
+      puts 'Buzz'
+    else
+      puts number
+    end
+  end
+end
+
+# Result
+fizz_buzz 15
